b3c01c1eedd80f637c26c9f733bade586c66ed2a
Алексей Зенков <zenkov.m700@mail.ru>
2024-06-30 22:57:06 +0300

    [LP-VSTU-6] Реализован раздел статистики вуза

--- src/App.vue
+++ src/App.vue
@@ -3,12 +3,14 @@
 import TheFirstScreen from "@/components/TheFirstScreen.vue";
 import TheDirectionsSection from "@/components/TheDirectionsSection.vue";
 import ThedDrectionStudy from "@/components/ThedDrectionStudy.vue";
+import TheInNumbers from "@/components/TheInNumbers.vue";
 </script>
 
 <template>
   <the-first-screen/>
   <the-directions-section/>
 <!--  <thed-drection-study/>-->
+  <the-in-numbers/>
 </template>
 
 <style scoped>
